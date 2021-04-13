649b1119c5c85e5978f4b094213e044239e626e7
UjjwalSingh1908 <ujjwal1911044@akgec.ac.in>
2021-04-14 03:23:17 +0530

    updated features swction

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Navbar transition transparent to solid
 $(window).scroll(function () {
   if ($(window).scrollTop() >= 50) {
     $("nav").addClass("navbar-solid");
@@ -5,3 +6,45 @@ $(window).scroll(function () {
     $("nav").removeClass("navbar-solid");
   }
 });
+
+// smooth scroll
+var $root = $('html, body');
+
+$('a[href^="#"]').click(function() {
+    var href = $.attr(this, 'href');
+
+    $root.animate({
+        scrollTop: $(href).offset().top
+    }, 500, function () {
+        window.location.hash = href;
+    });
+
+    return false;
+});
+
+// image Switch
+$(document).ready(function(){
+  $(".features-card").click(function(){
+      var imgId = "./assets/" + $(this).attr("id");
+      // console.log(imgId)
+      // Change src attribute of image
+      //  $(".s3-mobile-img").attr("src", imgId);
+
+       $('.s3-mobile-img').fadeOut(400, function(){
+  
+        $(this).attr('src',imgId).bind('onreadystatechange load', function(){
+              if (this.complete) $(this).fadeIn(400);
+        });
+      });
+      // 
+       if (window.matchMedia('(max-width: 425px)').matches)
+        {
+          $root.animate({
+            scrollTop: $('#features').offset().top
+        }, 500);
+        }
+       
+  })   
+})
+
+
